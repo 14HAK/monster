515f08de09f4d291957527bda1db665a174e7643
Dulon <dulonmolla88@gmail.com>
2025-01-22 23:56:30 +0600

    f-demo-react setup eslint and prettier

---
+++ f-demo-react/eslint.config.js
@@ -0,0 +1,29 @@
+import pluginJs from '@eslint/js';
+import airbnbTypescript from 'eslint-config-airbnb-typescript';
+import pluginReact from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	{
+		plugins: {
+			airbnbTypescript,
+			'react-hooks': reactHooksPlugin, // Define the plugin object
+		},
+	},
+	{
+		rules: {
+			...airbnbTypescript.rules,
+
+			'react-hooks/rules-of-hooks': 'error', // Ensures hooks are used correctly
+			'react-hooks/exhaustive-deps': 'warn', // Warns on missing dependencies in useEffect
+		},
+	},
+];
