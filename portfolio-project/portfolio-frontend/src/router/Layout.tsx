88840f14be3a19e86f0cd6d279f987ed91fa6041
Dulon <dulonmolla88@gmail.com>
2025-03-04 15:44:38 +0600

    some files modifications

--- portfolio-project/portfolio-frontend/src/router/Layout.tsx
+++ portfolio-project/portfolio-frontend/src/router/Layout.tsx
@@ -7,13 +7,13 @@ import Home from "../pages/Home";
 const Layout = (): React.ReactElement => {
   return (
     <>
-      <main className="max-h-screen w-screen font-serif">
+      <main className="w-screen max-h-screen font-serif">
         <SectionCom className="grid max-h-screen w-full grid-cols-12 overflow-hidden text-[#F8F2DE]">
-          <SectionCom className="col-span-5 max-h-screen w-full bg-gradient-to-tl from-slate-950 to-green-900">
+          <SectionCom className="w-full max-h-screen col-span-5 bg-gradient-to-tl from-slate-950 to-green-900">
             <Home />
           </SectionCom>
 
-          <SectionCom className="scrollbar-hide col-span-7 max-h-screen w-full overflow-y-auto bg-gradient-to-tr from-slate-950 to-green-900">
+          <SectionCom className="w-full max-h-screen col-span-7 overflow-y-auto scrollbar-hide bg-gradient-to-tr from-slate-950 to-green-900">
             <Outlet />
           </SectionCom>
         </SectionCom>
