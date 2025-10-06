98ac5c7fc17fc195cc7c2259e4cfff5564aa7dd8
14HAK <dulonmolla88@gmail.com>
2025-10-06 23:40:45 +0600

    js: variables types

--- oct-2025/js/index.js
+++ oct-2025/js/index.js
@@ -1,3 +1,28 @@
-// console log.
-console.log('manage my name: Dulon Mahadi Molla');
-console.log('Meta Forces.');
+// //* Phase 1:
+// console.log('manage my name: Dulon Mahadi Molla');
+// console.log('Meta Forces.');
+
+// // javascript history names: moca, ES5, ES6, ...
+// // variables declaration: var, let, const
+
+//* Phase 2:
+var name = 'dulon mahadi';
+console.log(name);
+
+var name = 'kamal';
+console.log(name);
+// var is changeable and re-declarable.
+
+let city = 'Dhaka';
+console.log(city);
+// let is changeable but not re-declarable.
+// not supported: let city = 'Dhaka'; then let city = 'Chittagong'; // error.
+
+city = 'cittagong';
+console.log(city);
+
+const country = 'bangladesh';
+console.log(country);
+
+// const is not changeable and not to be re-declarable.
+// const pi = 3.1416 then const pi = 4.1225; then pi = 2.2256; // error.
--- oct-2025/readme.md
+++ oct-2025/readme.md
@@ -3,7 +3,7 @@
 ```javascript
 video title: JavaScript Full Course | JavaScript - Learn Everything | Sheryians Coding School
 video link: https://www.youtube.com/watch?v=a-wVHL0lpb0
-timing: 00:00:00:01 [day:hour:minute:second]
+timing: 00:00:17:00 [day:hour:minute:second]
 ```
 
 ## demo-number
