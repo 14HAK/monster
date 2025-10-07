36aa2359d1cf7fafcc776cf1b53f60eb1bee13ad
14HAK <dulonmolla88@gmail.com>
2025-10-07 10:57:28 +0600

    phase 2,3,4,5: re viewd compleate

--- oct-2025/js/index.js
+++ oct-2025/js/index.js
@@ -5,24 +5,61 @@
 // // javascript history names: moca, ES5, ES6, ...
 // // variables declaration: var, let, const
 
-//* Phase 2:
-var name = 'dulon mahadi';
-console.log(name);
+// //* Phase 2:
+// var name = 'dulon mahadi';
+// console.log(name);
 
-var name = 'kamal';
-console.log(name);
-// var is changeable and re-declarable.
+// var name = 'kamal';
+// console.log(name);
+// // var is re-assignable and re-declarable.
 
-let city = 'Dhaka';
-console.log(city);
-// let is changeable but not re-declarable.
-// not supported: let city = 'Dhaka'; then let city = 'Chittagong'; // error.
+// let city = 'Dhaka';
+// console.log(city);
 
-city = 'cittagong';
-console.log(city);
+// city = 'cittagong';
+// console.log(city);
+// // let is re-assignable but not re-declarable.
+// // not supported: let city = 'Dhaka'; then let city = 'Chittagong'; // error.
 
-const country = 'bangladesh';
-console.log(country);
+// const country = 'bangladesh';
+// console.log(country);
+// // const is not re-assignable and not to be re-declarable.
+// // const pi = 3.1416 then const pi = 5.1225; then pi = 8.2256; // error.
 
-// const is not changeable and not to be re-declarable.
-// const pi = 3.1416 then const pi = 4.1225; then pi = 2.2256; // error.
+// // * Phase 3:
+// // Scope: Global scope, Function scope, Block scope.
+// // Global Scope: full window.
+// // Function Scope: behind function.
+// // Block Scope: behind block { ...  }. behind cali braces {}.
+// var name = 'dulon molla'; //global scope.
+
+// // var: global, function scoped.
+// // let, const: global, function, block scoped.
+
+// //* Phase 4:
+// // assignable and declarable.
+// // initialize:
+// var names = 'dulon mahadi';
+// // re assign:
+// names = 'dulon molla';
+// // re declare:
+// var names = 'dulon mahadi molla';
+
+// //* Phase 5:
+// Temporal Dead Zone (TDZ).
+console.log(age); // error: cannot access 'age' before initialization.
+let age = 35;
+// this is TDZ. so, always write clean and serialized code.
+
+// Hoisting:
+// hoisting means, moving declaration to the top and initialization stays in place.
+// suppose you write code,
+var salary = 50000;
+
+// when execute this code compiler read and write this code like this,
+var salary = undefined; // execute top of declaration
+salary = 50000; // execute in place of initialization
+// so this is called hoisting.
+
+// only declaration is hoisted, not the initialization.
+// var is hoisted, let and const are not hoisted.
