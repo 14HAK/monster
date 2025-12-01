fe8f3f4b81da9fff3e817f18cbca7b7525fd033e
14HAK <dulonmolla88@gmail.com>
2025-12-01 23:19:31 +0600

    freecodecamp dev course funny html project. recipe

---
+++ freecodecamp/1.build-a-recipe-page.js
@@ -0,0 +1,68 @@
+** start of index.html **
+
+<!Doctype html>
+<html lang="en">
+
+<head>
+  <title>simplr white cake</title>
+  <meta charset="utf-8">
+</head>
+
+<body>
+  <h1>Simple White Cake!</h1>
+  <p>This simple cake recipe was sent home from our children's school. It is the easiest, best-tasting cake I've ever
+    made! It's great to make with the kids, especially for cupcakes. Use your favorite frosting on top of this delicious
+    cake.</p>
+  <figure>
+    <img src="https://cdn.freecodecamp.org/curriculum/labs/recipe.jpg" alt="decorated simple white cake">
+    <figcaption></figcaption>
+  </figure>
+  <h2>Ingredients</h2>
+  <ul>
+    <li>1 cup white sugar</li>
+    <li>½ cup unsalted butter, softened</li>
+    <li>2 large eggs, room temperature</li>
+    <li>2 teaspoons vanilla extract</li>
+    <li>1 ½ cups all-purpose flour</li>
+    <li>1 ¾ teaspoons baking powder</li>
+    <li>1/4 teaspoon table salt</li>
+    <li>½ cup whole milk, room temperature</li>
+  </ul>
+  <h2>Instructions</h2>
+  <ol>
+    <li>Gather all ingredients. Preheat the oven to 350 degrees F (175 degrees C). Grease and flour a 9-inch square
+      cake pan.</li>
+    <li>Beat sugar and butter together in a mixing bowl with an electric mixer until lighter in color and fluffy, 3 to
+      4 minutes. Add eggs, one at a time, beating briefly after each addition, 30 seconds total. Mix in vanilla, about
+      15 seconds.</li>
+    <li>Whisk flour, baking powder, and salt in a separate bowl. With mixer on low speed, add flour mixture to butter
+      mixture in 3 batches, alternating with milk, beginning and ending with flour. Mix just until combined stopping
+      to scrape down sides if needed, about 2 minutes.</li>
+    <li>Spread cake batter into the prepared pan.</li>
+    <li>Bake cake in the preheated oven until a toothpick inserted into the center comes out clean, about 30 minutes.
+    </li>
+    <li>Remove cake from the oven and let cool in pan on a wire rack for 10 minutes. Invert cake onto wire rack;
+      remove pan and let cake cool completely before frosting. Enjoy!</li>
+  </ol>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </ul>
+</body>
+
+</html>
+
+** end of index.html **
+
